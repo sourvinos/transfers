2d5953507541d2224f59f322c7e396d737031cb2
John Sourvinos <johnsourvinos@hotmail.com>
2020-01-09 11:30:50 +0200

    CSS tweaks

--- ClientApp/src/app/root/root.component.ts
+++ ClientApp/src/app/root/root.component.ts
@@ -41,8 +41,8 @@ export class RootComponent implements AfterViewInit {
     }
 
     private positionLoader() {
-        document.getElementById('spinner').style.left = (document.getElementById('main').clientWidth / 2) - (document.getElementById('spinner').clientWidth / 2) + document.getElementById('sidebar').clientWidth + 'px'
-        document.getElementById('spinner').style.top = (document.getElementById('main').clientHeight / 2) - document.getElementById('spinner').clientHeight / 2 + 'px'
+        document.getElementById('spinner').style.left = (window.screen.width / 2) - (document.getElementById('spinner').clientWidth / 2) + 'px'
+        document.getElementById('spinner').style.top = (document.getElementById('main').clientHeight / 2) - (document.getElementById('spinner').clientHeight / 2) + 'px'
     }
 
 }
