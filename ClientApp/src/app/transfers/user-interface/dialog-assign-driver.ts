60894cabac8807585b72e9ce56779e876ebaa673
John Sourvinos <johnsourvinos@hotmail.com>
2020-01-22 11:05:12 +0200

    Replaces the driver select dropdown with autocomplete

--- ClientApp/src/app/transfers/user-interface/dialog-assign-driver.ts
+++ ClientApp/src/app/transfers/user-interface/dialog-assign-driver.ts
@@ -1,5 +1,8 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { IDriver } from './../../models/driver';
 
 @Component({
@@ -10,19 +13,76 @@ import { IDriver } from './../../models/driver';
 
 export class DialogAssignDriverComponent {
 
-    driverId: number
+    id: string = ''
+    driverDescription = new FormControl()
     drivers: IDriver[] = []
+    filteredDrivers: Observable<IDriver[]>
 
     constructor(private dialogRef: MatDialogRef<DialogAssignDriverComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
     ngOnInit() {
+        this.populateDropDowns()
+        this.trackChangesInAutoComplete()
+    }
+
+    close() {
+        this.dialogRef.close()
+    }
+
+    /**
+     * Caller(s):
+     *  Template - Autocomplete
+     * 
+     * Description:
+     *  Updates the id of the driver
+     * 
+     * @param event 
+     */
+    onSelectionChanged(event: { option: { id: string; }; }) {
+        this.id = event.option.id
+    }
+
+    /**
+     * Caller(s):
+     *  Class - trackChangesInAutoComplete
+     * 
+     * Description:
+     *  Filters the array according to the given characters
+     * 
+     * @param description 
+     */
+    private filter(description: string): IDriver[] {
+        const filterValue = description.toLowerCase()
+        return this.drivers.filter(option => option.description.toLowerCase().indexOf(filterValue) === 0)
+    }
+
+    /**
+     * Caller(s):
+     *  Class - ngOnInit()
+     * 
+     * Description:
+     *  Self-explanatory
+     */
+    private populateDropDowns() {
         this.data.drivers.subscribe((result: any) => {
             this.drivers = result
         })
     }
 
-    close() {
-        this.dialogRef.close()
+    /**
+     * Caller(s):
+     *  Class - ngOnInit()
+     * 
+     * Description:
+     *  Keeps track of what is given in the dropdown and filters the array
+     */
+    private trackChangesInAutoComplete() {
+        this.filteredDrivers = this.driverDescription.valueChanges
+            .pipe(
+                startWith(''),
+                map(value => typeof value === 'string' ? value : value.abbreviation),
+                map(input => input ? this.filter(input) : this.drivers.slice())
+            )
     }
 
 }
